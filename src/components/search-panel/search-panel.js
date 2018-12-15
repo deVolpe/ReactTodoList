6704052c8475f21bffa7511c363124ad86826dd7
deVolpe <deVolpe@users.noreply.github.com>
2018-12-15 18:36:04 +0300

    add: Searching Todo item by term

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 
 import "./search-panel.css"
 
-export const SearchPanel = () => {
-  return (
-    <div className="search-input">
-      <input type="text" className="form-control" placeholder="type of search"/>
-    </div>
-  );
+
+export default class SearchPanel extends Component {
+
+  state = {
+    term: ''
+  };
+
+  onSearchChange = (e) => {
+    const term = e.target.value;
+    this.setState({ term });
+    this.props.onSearchChange(term);
+  };
+
+  render() {
+
+    const { term } = this.state;
+
+    return (
+      <div className="search-input">
+        <input type="text"
+           className="form-control"
+           value={term}
+           placeholder="type of search"
+           onChange={this.onSearchChange}/>
+      </div>
+    );
+  };
 }
