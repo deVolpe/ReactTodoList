b3ab9f0f42aab809ff6591086661a0294d3d35a9
deVolpe <deVolpe@users.noreply.github.com>
2018-12-17 10:52:53 +0300

    add: filter elements by active and done

--- src/components/item-status-filter/item-status-filter.js
+++ src/components/item-status-filter/item-status-filter.js
@@ -4,13 +4,45 @@ import "./item-status-filter.css";
 
 export default class ItemStatusFilter extends Component {
 
+  state = {
+    active: true,
+    done: true
+  };
+
+  onFilterActive = () => {
+    const {active} = this.state;
+    this.setState({
+      active: !active
+    });
+    this.props.onFilterActive(active);
+  };
+
+  onFilterDone = () => {
+    const {done} = this.state;
+    this.setState({
+      done: !done
+    });
+    this.props.onFilterDone(done);
+  };
+
   render() {
-    return (
-      <div className="btn-group">
-        <button className="btn btn-info">All</button>
-        <button className="btn btn-outline-secondary">Active</button>
-        <button className="btn btn-outline-secondary">Done</button>
-      </div>
-    );
+
+    const {active, done} = this.state;
+
+    let firstClassName = "btn btn-outline-secondary";
+    let secondClassName = "btn btn-outline-secondary";
+    if (!active) {
+      firstClassName = "btn btn-outline-success";
+    }
+
+    if (!done) {
+      secondClassName = "btn btn-outline-success";
+    }
+
+    return (<div className="btn-group">
+      <button className="btn btn-info">All</button>
+      <button className={firstClassName} onClick={this.onFilterActive}>Active</button>
+      <button className={secondClassName} onClick={this.onFilterDone}>Done</button>
+    </div>);
   };
 }
